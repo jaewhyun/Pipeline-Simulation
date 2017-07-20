8becd08b367c950abd3f75a9287322258474bbe5
jaewhyun <jaewhyun@gmail.com>
2017-07-20 19:03:30 +0900

    Create pipeline.cpp

---
+++ pipeline.cpp
@@ -0,0 +1,698 @@
+
+Click here to enable desktop notifications for Boston University Mail.   Learn more  Hide
+Mail
+COMPOSE
+Labels
+Inbox (974)
+Starred
+Sent Mail
+Drafts (16)
+More 
+Hangouts
+
+ 
+ 
+Move to Inbox More 
+1 of 7  
+ 
+Collapse all Print all In new window
+CS472 - project 3 submission 
+Inbox
+x 
+
+Jae Won Hyun <jaehyun@bu.edu>
+Attachments12/2/16
+
+to Karen 
+Hi Karen,
+
+Hope you had a great Thanksgiving weekend! Phewf, just making it in before the time is up! 
+
+Here are my source codes for the project.
+
+Thanks so much,
+Jae
+11 Attachments 
+ 
+
+kspalmer@bu.edu
+12/2/16
+
+to hendrick, me 
+Hi Jae,
+
+Would you also, as the project requires, please send me the output from your program? You can just copy and paste the output into a text document.
+
+Thanks,
+Karen.
+
+Sent from my iPhone
+
+> On Dec 1, 2016, at 23:54, Jae Won Hyun <jaehyun@bu.edu> wrote:
+>
+> Hi Karen,
+>
+> Hope you had a great Thanksgiving weekend! Phewf, just making it in before the time is up!
+>
+> Here are my source codes for the project.
+>
+> Thanks so much,
+> Jae
+> <exmem.h>
+> <idex.cpp>
+> <idex.h>
+> <ifid.cpp>
+> <ifid.h>
+> <main.cpp>
+> <memwb.h>
+> <pipeline.cpp>
+> <pipeline.h>
+> <exmem.cpp>
+> <memwb.cpp>
+
+Jae Won Hyun <jaehyun@bu.edu>
+Attachments12/3/16
+
+to Karen 
+Hi Karen,
+
+So sorry! Here it is.
+
+Thanks so much,
+Jae
+
+Attachments area
+
+Jae Won Hyun <jaehyun@bu.edu>
+Attachments12/3/16
+
+to David 
+
+
+---------- Forwarded message ----------
+From: Jae Won Hyun <jaehyun@bu.edu>
+Date: Thu, Dec 1, 2016 at 11:54 PM
+Subject: CS472 - project 3 submission
+To: Karen Palmer <kspalmer@bu.edu>
+
+
+Hi Karen,
+
+Hope you had a great Thanksgiving weekend! Phewf, just making it in before the time is up! 
+
+Here are my source codes for the project.
+
+Thanks so much,
+Jae
+
+12 Attachments 
+ 
+
+Karen Palmer <kspalmer@bu.edu>
+Attachments12/10/16
+
+to david.hendrick., me 
+Hi Jae - 
+
+Great job on project 3 - you earned a 90%. 
+
+(10 points off) You read or wrote to Main Mem in a stage other than MEM.  You read from Main Mem in the WB stage.
+
+You had a great semester with your projects!
+All the best,
+Karen.
+
+
+
+---------- Forwarded message ----------
+From: Jae Won Hyun <jaehyun@bu.edu>
+Date: Fri, Dec 2, 2016 at 10:21 AM
+Subject: Re: CS472 - project 3 submission
+To: Karen Palmer <kspalmer@bu.edu>
+
+
+Hi Karen,
+
+So sorry! Here it is.
+
+Thanks so much,
+Jae
+
+On Fri, Dec 2, 2016 at 7:15 AM, <kspalmer@bu.edu> wrote:
+Hi Jae,
+
+Would you also, as the project requires, please send me the output from your program? You can just copy and paste the output into a text document.
+
+Thanks,
+Karen.
+
+Sent from my iPhone
+
+> On Dec 1, 2016, at 23:54, Jae Won Hyun <jaehyun@bu.edu> wrote:
+>
+> Hi Karen,
+>
+> Hope you had a great Thanksgiving weekend! Phewf, just making it in before the time is up!
+>
+> Here are my source codes for the project.
+>
+> Thanks so much,
+> Jae
+> <exmem.h>
+> <idex.cpp>
+> <idex.h>
+> <ifid.cpp>
+> <ifid.h>
+> <main.cpp>
+> <memwb.h>
+> <pipeline.cpp>
+> <pipeline.h>
+> <exmem.cpp>
+> <memwb.cpp>
+
+
+
+
+-- 
+All the best,
+Karen Palmer
+
+(508) 789-0903
+kspalmer@bu.edu
+
+Twenty years from now you will be more disappointed by the things you didn't do than by the ones you did do.  So throw off the bowlines.  Sail away from the safe harbor.  Catch the trade winds in your sails.  Explore. Dream.  Discover.
+       ~ Mark Twain
+Attachments area
+
+Jae Won Hyun <jaehyun@bu.edu>
+12/20/16
+
+to Karen 
+Hi Karen,
+
+I just wanted to thank you for an awesome semester. I really appreciate all the feedback and I learned a lot!
+
+Best,
+Jae
+
+
+kspalmer@bu.edu
+12/20/16
+
+to me 
+Thank you, Jae!  I really appreciate you writing! 
+
+All the best to you!
+
+Karen. 
+
+Sent from my iPhone
+
+On Dec 20, 2016, at 08:17, Jae Won Hyun <jaehyun@bu.edu> wrote:
+
+Hi Karen,
+
+I just wanted to thank you for an awesome semester. I really appreciate all the feedback and I learned a lot!
+
+Best,
+Jae
+
+
+
+On Dec 9, 2016 2:54 PM, "Karen Palmer" <kspalmer@bu.edu> wrote:
+Hi Jae - 
+
+Great job on project 3 - you earned a 90%. 
+
+(10 points off) You read or wrote to Main Mem in a stage other than MEM.  You read from Main Mem in the WB stage.
+
+You had a great semester with your projects!
+All the best,
+Karen. 
+
+---------- Forwarded message ----------
+From: Jae Won Hyun <jaehyun@bu.edu>
+Date: Fri, Dec 2, 2016 at 10:21 AM
+Subject: Re: CS472 - project 3 submission
+To: Karen Palmer <kspalmer@bu.edu>
+
+
+Hi Karen,
+
+So sorry! Here it is.
+
+Thanks so much,
+Jae
+
+On Fri, Dec 2, 2016 at 7:15 AM, <kspalmer@bu.edu> wrote:
+Hi Jae,
+
+Would you also, as the project requires, please send me the output from your program? You can just copy and paste the output into a text document.
+
+Thanks,
+Karen.
+
+Sent from my iPhone
+
+> On Dec 1, 2016, at 23:54, Jae Won Hyun <jaehyun@bu.edu> wrote:
+>
+> Hi Karen,
+>
+> Hope you had a great Thanksgiving weekend! Phewf, just making it in before the time is up!
+>
+> Here are my source codes for the project.
+>
+> Thanks so much,
+> Jae
+> <exmem.h>
+> <idex.cpp>
+> <idex.h>
+> <ifid.cpp>
+> <ifid.h>
+> <main.cpp>
+> <memwb.h>
+> <pipeline.cpp>
+> <pipeline.h>
+> <exmem.cpp>
+> <memwb.cpp>
+
+
+
+
+-- 
+All the best,
+Karen Palmer
+
+(508) 789-0903
+kspalmer@bu.edu
+
+Twenty years from now you will be more disappointed by the things you didn't do than by the ones you did do.  So throw off the bowlines.  Sail away from the safe harbor.  Catch the trade winds in your sails.  Explore. Dream.  Discover.
+       ~ Mark Twain
+	
+Click here to Reply or Forward
+Using 0.13 GB
+Manage
+Program Policies
+Powered by Google
+Last account activity: Jul 2
+Details
+People (3)
+Karen Palmer
+kspalmer@bu.edu
+
+Show details
+
+
+#include <stdio.h>
+#include "pipeline.h"
+#include "ifid.h"
+#include "idex.h"
+#include "exmem.h"
+#include "memwb.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+pipeline::pipeline()
+{
+    int count = 0;
+    
+    while(count < mainmemory)
+    {
+        for(short i = 0; i < 0xFF + 1; i++)
+        {
+            Main_Mem[count] = i;
+            //cout << "main_mem[" << hex << count << "] =";
+            count++;
+            //cout << hex << main_mem[i] << "\n";
+        }
+    }
+    
+    for(int i = 1; i < 32; i++)
+    {
+        Regs[0] = 0;
+        
+        Regs[i] = 0x100 + i;
+    }
+}
+
+// read from instruction set and write into ifidwrite
+void pipeline::if_stage(int instruction)
+{
+    ifidwrite.setinst(instruction);
+}
+
+// read from ifidread, write into idexwrite
+void pipeline::id_stage()
+{
+    int instruction;
+    instruction = ifidread.getinst();
+    
+    if(instruction == 0)
+    {
+        idexwrite.setregdst(0);
+        idexwrite.setalusrc(0);
+        idexwrite.setaluop(0);
+        idexwrite.setmemread(0);
+        idexwrite.setmemwrite(0);
+        idexwrite.setmemtoreg(0);
+        idexwrite.setregwrite(0);
+        
+        idexwrite.seoffset(0);
+        idexwrite.setreadreg1(0);
+        idexwrite.setreadreg2(0);
+        idexwrite.writereg1511(0);
+        idexwrite.writereg2016(0);
+        idexwrite.setfunction(0);
+    }
+    else
+    {
+        // calc opcode
+        int opcode = 0;
+        opcode = (ifidread.getinst() & 0xFC000000) >> 26;
+
+        // calc offset
+        int SEOffset = 0;
+        SEOffset = (ifidread.getinst() & 0x0000FFFF);
+        idexwrite.seoffset(SEOffset);
+        
+        // get register 1 number and value
+        int sRegister1 = 0;
+        sRegister1 = (ifidread.getinst() & 0x03E00000) >> 21;
+        idexwrite.setreadreg1(Regs[sRegister1]);
+        
+        // get register 2 number and value
+        // writereg_20_16
+        int sRegister2 = 0;
+        sRegister2 = (ifidread.getinst() & 0x001F0000) >> 16;
+        idexwrite.setreadreg2(Regs[sRegister2]);
+        
+        // get register 3 number
+        // writereg_15_11
+        int sRegister3 = 0;
+        sRegister3 = (ifidread.getinst() & 0x0000F800) >> 11;
+        
+        // set writereg2016 and 1511 values
+        idexwrite.writereg2016(sRegister2);
+        idexwrite.writereg1511(sRegister3);
+        
+        // set function
+        int function = 0;
+        function = (ifidread.getinst() & 0x0000003F);
+        idexwrite.setfunction(function);
+        
+        // R type
+        if(opcode == 0)
+        {
+            idexwrite.setregdst(1);
+            idexwrite.setalusrc(0);
+            idexwrite.setaluop(10);
+            idexwrite.setmemread(0);
+            idexwrite.setmemwrite(0);
+            idexwrite.setmemtoreg(0);
+            idexwrite.setregwrite(1);
+        }
+        // lb
+        else if(opcode == 0x20)
+        {
+            idexwrite.setregdst(0);
+            idexwrite.setalusrc(1);
+            idexwrite.setaluop(00);
+            idexwrite.setmemread(1);
+            idexwrite.setmemwrite(0);
+            idexwrite.setmemtoreg(1);
+            idexwrite.setregwrite(1);
+        }
+        // sb
+        else if(opcode == 0x28)
+        {
+            idexwrite.setalusrc(1);
+            idexwrite.setaluop(00);
+            idexwrite.setmemread(0);
+            idexwrite.setmemwrite(1);
+            // memtoreg technically doesn't do anything
+            idexwrite.setregwrite(0);
+        }
+    }
+}
+
+void pipeline::ex_stage()
+{
+    // nop
+    if(idexwrite.getregdst() == 0 && idexwrite.getalusrc() && idexwrite.getaluop() && idexwrite.getmemread() && idexwrite.getmemwrite() && idexwrite.getmemtoreg() && idexwrite.getregwrite())
+    {
+        exmemwrite.setmemread(0);
+        exmemwrite.setmemwrite(0);
+        exmemwrite.setmemtoreg(0);
+        exmemwrite.setregwrite(0);
+    }
+    else
+    {
+        // get control bits to pass onto next register
+        exmemwrite.setmemread(idexread.getmemread());
+        exmemwrite.setmemwrite(idexread.getmemwrite());
+        exmemwrite.setmemtoreg(idexread.getmemtoreg());
+        exmemwrite.setregwrite(idexread.getregwrite());
+        
+        int aluresult = 0;
+        
+        // using the control bits regdst, alusrc, aluop
+        // set aluresult
+        // rtype
+        if(idexread.getaluop() == 10 && idexread.getalusrc() == 0 && idexread.getregdst() == 1)
+        {
+            // subtract
+            if(idexread.getfunction() == 0x22)
+            {
+                aluresult = idexread.getreadreg1() - idexread.getreadreg2();
+                exmemwrite.setaluresult(aluresult);
+            }
+            // add
+            else if(idexread.getfunction() == 0x20)
+            {
+                aluresult = idexread.getreadreg1() + idexread.getreadreg2();
+                exmemwrite.setaluresult(aluresult);
+            }
+            
+            // set swvalue
+            int swvalue = 0;
+            swvalue = idexread.getreadreg2();
+            exmemwrite.setswvalue(swvalue);
+            
+            // destination register
+            int writeregnum = 0;
+            writeregnum = idexread.getwritereg1511();
+            exmemwrite.setwriteregnum(writeregnum);
+            
+        }
+        // lb
+        else if(idexread.getaluop() == 00 && idexread.getalusrc() == 1 && idexread.getregdst() == 0)
+        {
+            aluresult = idexread.getreadreg1() + idexread.getoffset();
+            exmemwrite.setaluresult(aluresult);
+            
+            int writeregnum = 0;
+            writeregnum = idexread.getwritereg2016();
+            exmemwrite.setwriteregnum(writeregnum);
+            
+            // set swvalue
+            int swvalue = 0;
+            swvalue = idexread.getreadreg2();
+            exmemwrite.setswvalue(swvalue);
+        }
+        // sb
+        else if(idexread.getaluop() == 0 && idexread.getalusrc() == 1)
+        {
+            aluresult = idexread.getreadreg1() + idexread.getoffset();
+            exmemwrite.setaluresult(aluresult);
+            
+            // set swvalue
+            int swvalue = 0;
+            swvalue = idexread.getreadreg2();
+            exmemwrite.setswvalue(swvalue);
+        }
+        // if nop
+        else
+        {
+            aluresult = 0;
+            exmemwrite.setaluresult(0);
+            exmemwrite.setwriteregnum(0);
+            exmemwrite.setswvalue(0);
+        }
+    }
+    
+  
+}
+
+void pipeline::mem_stage()
+{
+    // get control bits
+    memwbwrite.setmemtoreg(exmemread.getmemtoreg());
+    memwbwrite.setregwrite(exmemread.getregwrite());
+    
+    // setaluresult
+    memwbwrite.setaluresult(exmemread.getaluresult());
+    //memwbwrite.setwriteregnum(0);
+    //memwbwrite.setlwdatavalue(0);
+    
+    // nop
+    if(exmemread.getmemread() == 0 && exmemread.getmemwrite() == 0 && exmemread.getmemtoreg() ==0 && exmemread.getregwrite() == 0)
+    {
+        memwbwrite.setwriteregnum(0);
+        memwbwrite.setlwdatavalue(0);
+    }
+    // lb
+    else if(exmemread.getmemread() == 1 && exmemread.getmemwrite() == 0)
+    {
+        memwbwrite.setlwdatavalue(Main_Mem[(exmemread.getaluresult())]);
+        memwbwrite.setwriteregnum(exmemread.getwriteregnum());
+    }
+    // sb
+    else if(exmemread.getmemread() == 0 && exmemread.getmemwrite() == 1)
+    {
+        Main_Mem[exmemread.getaluresult()] = exmemread.getswvalue();
+    }
+    // r type
+    else if(exmemread.getmemread() == 0 && exmemread.getmemwrite() == 0) // add or subtract
+    {
+        memwbwrite.setwriteregnum(exmemread.getwriteregnum());
+        
+    }
+}
+
+void pipeline::wb_stage()
+{
+    // lw
+    if(memwbread.getmemtoreg() == 1 && memwbread.getregwrite() == 1)
+    {
+        Regs[memwbread.getwriteregnum()] = Main_Mem[memwbread.getaluresult()];
+    }
+    // add or subtract
+    else if(memwbread.getmemtoreg() == 0 && memwbread.getregwrite()== 1)
+    {
+        Regs[memwbread.getwriteregnum()] = memwbread.getaluresult();
+    }
+    // sw nothing happens
+}
+
+void pipeline::print_out_everything()
+{
+    for(size_t i =  0; i < 32; i++)
+    {
+        cout << "Regs[" << dec << i <<"]: 0x" << uppercase <<  hex << Regs[i] << "\n";
+    }
+    
+    // printifid
+    cout << "\nIF/ID Write";
+    cout << "\nInst: 0x" << setfill('0') << setw(8) << uppercase << hex << ifidwrite.getinst() << "\n";
+    
+    cout << "\nIF/ID Read";
+    cout << "\nInst: 0x" << setfill('0') << setw(8) << uppercase << hex << ifidread.getinst() << "\n";
+    
+    // printidex
+    cout << "\nID/EX Write";
+    cout << "\nRegDst: " << idexwrite.getregdst();
+    cout << "\nALUSrc: " << idexwrite.getalusrc();
+    cout << "\nALUOp: " << setfill('0') << setw(2) << dec << idexwrite.getaluop();
+    cout << "\nMemRead: " << idexwrite.getmemread();
+    cout << "\nMemWrite: " << idexwrite.getmemwrite();
+    cout << "\nMemToReg: " << idexwrite.getmemtoreg();
+    cout << "\nRegWrite: " << idexwrite.getregwrite();
+    cout << "\nReadReg1Value: 0x" << uppercase << hex << idexwrite.getreadreg1();
+    cout << "\nReadReg2Value: 0x" << uppercase << hex << idexwrite.getreadreg2();
+    cout << "\nSEOffest: 0x" << uppercase << hex << idexwrite.getoffset();
+    cout << "\nWriteReg_20_16: " << dec << idexwrite.getwritereg2016();
+    cout << "\nWriteReg_15_11: " << dec << idexwrite.getwritereg1511();
+    cout << "\nFunction: 0x" << uppercase << hex << idexwrite.getfunction() << "\n";
+    
+    // printidex
+    cout << "\nID/EX Read";
+    cout << "\nRegDst: " << idexread.getregdst();
+    cout << "\nALUSrc: " << idexread.getalusrc();
+    cout << "\nALUOp: " << setfill('0') << setw(2) << dec << idexread.getaluop();
+    cout << "\nMemRead: " << idexread.getmemread();
+    cout << "\nMemWrite: " << idexread.getmemwrite();
+    cout << "\nMemToReg: " << idexread.getmemtoreg();
+    cout << "\nRegWrite: " << idexread.getregwrite();
+    cout << "\nReadReg1Value: 0x" << uppercase << hex << idexread.getreadreg1();
+    cout << "\nReadReg2Value: 0x" << uppercase << hex << idexread.getreadreg2();
+    cout << "\nSEOffest: 0x" << uppercase << hex << idexread.getoffset();
+    cout << "\nWriteReg_20_16: " << dec << idexread.getwritereg2016();
+    cout << "\nWriteReg_15_11: " << dec << idexread.getwritereg1511();
+    cout << "\nFunction: 0x" << uppercase << hex << idexread.getfunction() << "\n";
+    
+    // print exmem
+    cout << "\nEX/MEM Write";
+    cout << "\nMemRead: " << exmemwrite.getmemread();
+    cout << "\nMemWrite: " << exmemwrite.getmemwrite();
+    cout << "\nMemToReg: " << exmemwrite.getmemtoreg();
+    cout << "\nRegWrite: " << exmemwrite.getregwrite();
+    cout << "\nALUResult: 0x" << uppercase << hex << exmemwrite.getaluresult();
+    cout << "\nSWValue: 0x" << uppercase << hex << exmemwrite.getswvalue();
+    cout << "\nWriteRegNum: " << dec << exmemwrite.getwriteregnum()<< "\n";
+    
+    cout << "\nEX/MEM Read";
+    cout << "\nMemRead: " << exmemread.getmemread();
+    cout << "\nMemWrite: " << exmemread.getmemwrite();
+    cout << "\nMemToReg: " << exmemread.getmemtoreg();
+    cout << "\nRegWrite: " << exmemread.getregwrite();
+    cout << "\nALUResult: 0x" << uppercase << hex << exmemread.getaluresult();
+    cout << "\nSWValue: 0x" << uppercase << hex << exmemread.getswvalue();
+    cout << "\nWriteRegNum: " << dec << exmemread.getwriteregnum()<< "\n";
+    
+    // print memwbwrite
+    cout << "\nMEM/WB Write";
+    cout << "\nMemToReg: " << memwbwrite.getmemtoreg();
+    cout << "\nRegWrite: " << memwbwrite.getregwrite();
+    cout << "\nLWDataValue: " << dec << memwbwrite.getlwdatavalue();
+    cout << "\nALUResult: 0x" << uppercase << hex << memwbwrite.getaluresult();
+    cout << "\nWriteRegNum: "  << dec << memwbwrite.getwriteregnum() << "\n";
+    
+    cout << "\nMEM/WB Read";
+    cout << "\nMemToReg: " << memwbread.getmemtoreg();
+    cout << "\nRegWrite: " << memwbread.getregwrite();
+    cout << "\nLWDataValue: " << dec << memwbread.getlwdatavalue();
+    cout << "\nALUResult: 0x" << uppercase << hex << memwbread.getaluresult();
+    cout << "\nWriteRegNum: " << dec << memwbread.getwriteregnum() << "\n";
+}
+
+void pipeline::copy_write_to_read()
+{
+    // from ifidwriteto ifidread
+    ifidread.setinst(ifidwrite.getinst());
+    
+    // from idexwrite to idexread
+    // copy control
+    idexread.setregdst(idexwrite.getregdst());
+    idexread.setalusrc(idexwrite.getalusrc());
+    idexread.setaluop(idexwrite.getaluop());
+    idexread.setmemread(idexwrite.getmemread());
+    idexread.setmemwrite(idexwrite.getmemwrite());
+    idexread.setmemtoreg(idexwrite.getmemtoreg());
+    idexread.setregwrite(idexwrite.getregwrite());
+    // copy values
+    idexread.setreadreg1(idexwrite.getreadreg1());
+    idexread.setreadreg2(idexwrite.getreadreg2());
+    idexread.seoffset(idexwrite.getoffset());
+    idexread.writereg2016(idexwrite.getwritereg2016());
+    idexread.writereg1511(idexwrite.getwritereg1511());
+    idexread.setfunction(idexwrite.getfunction());
+    
+    
+    // from exmemwrite to exmemread
+    exmemread.setmemread(exmemwrite.getmemread());
+    exmemread.setmemwrite(exmemwrite.getmemwrite());
+    exmemread.setmemtoreg(exmemwrite.getmemtoreg());
+    exmemread.setregwrite(exmemwrite.getregwrite());
+    exmemread.setaluresult(exmemwrite.getaluresult());
+    exmemread.setswvalue(exmemwrite.getswvalue());
+    exmemread.setwriteregnum(exmemwrite.getwriteregnum());
+    
+    // from memwbwrite to memwbread
+    memwbread.setmemtoreg(memwbwrite.getmemtoreg());
+    memwbread.setregwrite(memwbwrite.getregwrite());
+    memwbread.setlwdatavalue(memwbwrite.getlwdatavalue());
+    memwbread.setaluresult(memwbwrite.getaluresult());
+    memwbread.setwriteregnum(memwbwrite.getwriteregnum());
+}
